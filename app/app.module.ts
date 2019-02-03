b00c4a039b0b6dc44f561366af3f74d8487f54a6
NabidAlam <msa.nabid007@gmail.com>
2019-02-04 01:56:34 +0600

    nativescript ui chart

---
+++ app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { CommonDirectivesModule } from './navigation/directives/common-directives.module';
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { OptionsService } from "./navigation/options/options.service";
+import { ExampleItemService } from "./navigation/exampleItemService.service";
+import { ExamplesListDepthComponents } from "./navigation/examples-list/examples-list.component";
+import { OptionsComponent } from "./navigation/options/options.component";
+import { routing } from "./app.routing";
+import { AppComponent } from "./navigation/app.component";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        CommonDirectivesModule,
+        NativeScriptFormsModule,
+        NativeScriptRouterModule,
+        routing
+    ],
+    exports: [
+        NativeScriptModule,
+        NativeScriptRouterModule
+    ],
+    declarations: [
+        AppComponent,
+        ExamplesListDepthComponents,
+        OptionsComponent,
+    ],
+    providers: [
+        OptionsService,
+        ExampleItemService
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
